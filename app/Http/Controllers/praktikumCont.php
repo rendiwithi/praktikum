1072f75404fa95bc9d93a04f380c3c48dc8425e1
Yosafat Fiken <yosafatfiken@gmail.com>
2022-06-07 15:16:20 +0700

    second commit

---
+++ app/Http/Controllers/praktikumCont.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class praktikumCont extends Controller
+{
+    public function kalabAslab() {
+        return view('kalabAslab');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/kalab', 'App\Http\Controllers\praktikumCont@kalabAslab');
+Route::get('/aslab', 'App\Http\Controllers\praktikumCont@kalabAslab');
